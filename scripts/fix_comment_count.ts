4a729a87f9284e0d19242e1fe75c14b7f6c676bb
huy.pham@inapps.net <huy.pham@inapps.net>
2021-01-04 11:15:44 +0700

    UPDATE

--- scripts/fix_comment_count.ts
+++ scripts/fix_comment_count.ts
@@ -1,6 +1,5 @@
 import { MONGODB } from '../server/configs/index.config';
 import mongoose from 'mongoose';
-import { ArticleModel, CommentModel } from '../test/models';
 
 const init = async () => {
     const conn = await mongoose.connect(MONGODB.uri, {
@@ -10,14 +9,7 @@ const init = async () => {
         useUnifiedTopology: true,
     });
 
-    const articles = await ArticleModel.find({});
-
-    await Promise.all(
-        articles.map(async (item) => {
-            const count = await CommentModel.countDocuments({ article: item._id });
-            await ArticleModel.updateOne({ _id: item._id }, { commentCount: count });
-        })
-    );
+    await Promise.all([]);
 
     conn.connection.close();
 
